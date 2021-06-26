ac5d0e8932a020a7d828843c7297a98bfb10f3d5
Jedrzej Tymiec <jedrek1692@gmail.com>
2021-06-26 21:49:54 +0200

    auth path

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from 'src/schemas/user.schema';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' }
+    }),
+    MongooseModule.forFeature([{name: 'User', schema: UserSchema}])
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService, JwtModule],
+  controllers: [AuthController]
+})
+export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/interfaces/users.interface';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private jwtService: JwtService,
+        @InjectModel("User") private readonly userModel: Model<User>
+        ) {}
+
+    async validateUser(email: string, pass: string): Promise<any> {
+        const user = await this.userModel.find({ email });
+        if (user.length > 0) {
+            
+            const match = await bcrypt.compare(pass, user[0].password)
+            if (match) {
+                const payload = { user: { id: user[0]._id } }
+                return {token: this.jwtService.sign(payload)}
+            }
+            throw new UnauthorizedException('Invalid Credentials')
+        }
+        throw new UnauthorizedException('Invalid Credentials')
+    }
+}
