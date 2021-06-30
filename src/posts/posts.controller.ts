2e0e7d5b7bbb69edd69d4575f569e185c8f51e07
Jedrzej Tymiec <jedrek1692@gmail.com>
2021-06-30 16:34:27 +0200

    delete post

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
+import {
+    Controller,
+    Post,
+    Body,
+    Request,
+    UseGuards,
+    Get,
+    Param,
+    Delete
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { AuthGuard } from '@nestjs/passport';
 import { PostInterface } from 'src/interfaces/post.interface';
@@ -10,7 +19,28 @@ export class PostsController {
 
   @UseGuards(AuthGuard('jwt'))
   @Post()
-  async createPost(@Body() postDto: PostDto, @Request() req): Promise<any> {
+  async createPost(@Body() postDto: PostDto, @Request() req): Promise<PostInterface> {
     return this.postsService.addNewPost(req.user.id, postDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  async getPosts(): Promise<PostInterface[]> {
+      return this.postsService.getAllPosts()
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get(':post_id')
+  async getPost(@Param('post_id') id: string): Promise<PostInterface> {
+      return this.postsService.getPostById(id)
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':post_id')
+  async dletePost(
+    @Param('post_id') id: string,
+    @Request() req
+  ): Promise<any> {
+      return this.postsService.deletePostById(req.user.id, id)
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { PostInterface } from 'src/interfaces/post.interface';
@@ -11,11 +11,35 @@ export class PostsService {
     @InjectModel('User') private readonly userModel: Model<UserInterface>,
   ) {}
 
-  async addNewPost(id, data): Promise<any> {
+  async addNewPost(id, data): Promise<PostInterface> {
     const { name, avatar } = await this.userModel
       .findById(id)
       .select('-password');
     const newPost = new this.postModel({ name, avatar, user: id, ...data });
     return await newPost.save();
+  }
+
+  async getAllPosts(): Promise<PostInterface[]> {
+      return await this.postModel.find().sort({ date: -1 })
+  }
+
+  async getPostById(id): Promise<PostInterface> {
+      const post = await this.postModel.findById(id)
+      if (!post) {
+          throw new BadRequestException('Post not found')
+      }
+      return post
+  }
+
+  async deletePostById(userId, postId): Promise<any> {
+    const post = await this.postModel.findById(postId)
+    if(!post) {
+        throw new BadRequestException('Post not found')
+    }
+    if(post.user != userId) {
+        throw new UnauthorizedException('User not authorized')
+    }
+    post.delete()
+    return { msg: 'Post deleted' }
   }
 }
